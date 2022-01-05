adc53980e2c5558019cbf11ac77bdb3887258e1e
gR <Gr>
2022-01-05 21:01:10 +0300

    add SwiftLint to project

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -9,11 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
-
